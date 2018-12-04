92493509aa11cb803f06c501a0ac07bbfdb7867f
nobswolf <github@bnm.me>
2018-12-04 06:15:17 +0100

    Update test_intfloat.py
    mehr Beispiele ... "Unit-Tests für arme" ;)

--- test_intfloat.py
+++ test_intfloat.py
@@ -1,9 +1,44 @@
 #!/usr/bin/python3
 
 from intfloat import *
+from decimal import *
+from fractions import *
 
-a = IntFloat(612,4)
+a = Fraction (321)
+b = Fraction (123)
+c = Fraction (345, 87)
+d = Fraction (3, 77)
 
-b = IntFloat(538,4)
+z = IntFloat ()
+print (z, z.periodic())
 
-print(a, b, a/b, a*b, a+b, a-b)
+z = IntFloat (a)
+print (z, z.periodic())
+
+z = IntFloat (a, 3)
+print (z, z.periodic())
+
+z = IntFloat (a, -4)
+print ("z =", z)
+z.negate()
+print ("-z","=", z)
+
+x = IntFloat (a, 3)
+y = IntFloat (b, -14)
+z = x+y
+print (x, "+", y, "=", z)
+
+x = IntFloat (a, 3)
+y = IntFloat (b, -14)
+z = x*y
+print (x, "*", y, "=", z)
+
+x = IntFloat (a, 3)
+y = IntFloat (b, -14)
+z = x/y
+print (x, "/", y, "=", z)
+
+z = IntFloat(d)
+z += IntFloat(Fraction(19,3))
+print (z, "=" , z.periodic())
+
